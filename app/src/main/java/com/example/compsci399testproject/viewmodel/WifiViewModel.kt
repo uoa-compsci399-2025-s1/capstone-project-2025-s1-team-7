7cb9f9b1a60af79d9ef4a2dfd5093a39e09173c9
ConnorWilliamsGIT <conjob.2010@gmail.com>
2025-04-12 01:49:41 +1200

    Created a wifiViewModel for loading into activities and handling calls to the wifiScanner

---
+++ app/src/main/java/com/example/compsci399testproject/viewmodel/WifiViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.compsci399testproject.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.net.wifi.ScanResult
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import com.example.compsci399testproject.utils.WifiScanner
+
+class WifiViewModel(application: Context) : AndroidViewModel(application as Application) {
+    private val scanner = WifiScanner(application.applicationContext)
+
+    private val _lastScanTime = mutableStateOf<Long?>(null)
+    val lastScanTime: State<Long?> = _lastScanTime
+
+    fun updateScanResults(results: List<ScanResult>) {
+        _lastScanTime.value = System.currentTimeMillis()
+        scanner.updateScanResults(results)
+    }
+
+    fun scan() {
+        scanner.scanWifi()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        scanner.cleanup()
+    }
+
+    fun getResults(): List<ScanResult> {
+        return scanner.scanResults.value
+    }
+}
