7bd5ebd49a52d4e80ceba4ff2c6ab849fe27e747
ConnorWilliamsGIT <conjob.2010@gmail.com>
2025-04-17 18:20:21 +1200

    Add functionality to read MAC addresses and calculate signal strength for access points into a float array

--- app/src/main/java/com/example/compsci399testproject/viewmodel/WifiViewModel.kt
+++ app/src/main/java/com/example/compsci399testproject/viewmodel/WifiViewModel.kt
@@ -3,10 +3,12 @@ package com.example.compsci399testproject.viewmodel
 import android.app.Application
 import android.content.Context
 import android.net.wifi.ScanResult
+import android.os.Environment
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import com.example.compsci399testproject.utils.WifiScanner
+import java.io.File
 
 class WifiViewModel(application: Context) : AndroidViewModel(application as Application) {
     private val scanner = WifiScanner(application.applicationContext, this)
@@ -16,9 +18,21 @@ class WifiViewModel(application: Context) : AndroidViewModel(application as Appl
     var scanResults = scanner.scanResults
     private var lastUploadedResults: Map<String, Int>? = null
 
+    private var accessPoints = mutableListOf<String>()
+    private var strengthArray = mutableListOf<Float>()
+
+    init {
+        val fileName = "macAddresses.csv"
+        val inputStream = application.assets.open(fileName)
+        val reader = inputStream.bufferedReader()
+        val firstLine = reader.readLine()
+        this.accessPoints = firstLine.split(",").toMutableList()
+    }
+
     fun updateScanResults() {
         _lastScanTime.value = System.currentTimeMillis()
         this.scanResults = scanner.scanResults
+        this.convertResultsToStrengthArray()
     }
 
     fun scan() {
@@ -43,5 +57,34 @@ class WifiViewModel(application: Context) : AndroidViewModel(application as Appl
             lastUploadedResults = currentMap
         }
         return changed
+    }
+
+    private fun convertResultsToStrengthArray(){
+        strengthArray.clear()
+        for (i in 0 until accessPoints.size) {
+            strengthArray.add(100f)
+        }
+        for (result in scanResults.value) {
+            val signalName = result.BSSID + "(${result.SSID})"
+            val level = result.level.toFloat()
+            val index = accessPoints.indexOf(signalName)
+            if (index != -1) {
+                strengthArray[index] = level
+            }
+        }
+//        this.writeStrengthArrayToFile()
+    }
+
+    fun getStrengthArray(): List<Float> {
+        return strengthArray
+    }
+
+    fun writeStrengthArrayToFile() {
+        val fileName = "strengthArray.csv"
+        val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val file = File(downloadsDir, fileName)
+        val writer = file.bufferedWriter()
+        writer.write(strengthArray.joinToString(","))
+        writer.close()
     }
 }
