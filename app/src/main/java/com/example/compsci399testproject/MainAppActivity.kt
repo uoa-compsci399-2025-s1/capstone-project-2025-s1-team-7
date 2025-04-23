e5c13e2b6b227cb74040c6d3eb2aa51d5bf80421
Eric <eale977@aucklanduni.ac.nz>
2025-04-23 19:05:17 +1200

    Renamed variables to camel case

--- app/src/main/java/com/example/compsci399testproject/MainAppActivity.kt
+++ app/src/main/java/com/example/compsci399testproject/MainAppActivity.kt
@@ -6,61 +6,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.geometry.Offset
 
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.graphics.asImageBitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -68,10 +41,8 @@ import androidx.compose.ui.unit.Dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 import com.example.compsci399testproject.viewmodel.MapViewModel
-import kotlinx.coroutines.launch
 import kotlin.math.PI
 import kotlin.math.cos
-import kotlin.math.floor
 import kotlin.math.sin
 
 
@@ -80,8 +51,8 @@ fun MapImageView(
     floor: Int,
     offset: Offset,
     onOffsetChange: (Offset) -> Unit,
-    positionX: Float,
-    positionY: Float,
+    positionXPercentage: Float,
+    positionYPercentage: Float,
     positionFloor: Int,
     rotation: Float
 ) {
@@ -114,13 +85,13 @@ fun MapImageView(
     var zoom by remember { mutableStateOf(3f) }
     var angle by remember { mutableStateOf(0f) }
 
-    val floor_image_size_width : Dp  = 300.dp
-    val floor_image_size_height : Dp = 300.dp
+    val floorImageSizeWidth : Dp  = 300.dp
+    val floorImageSizeHeight : Dp = 300.dp
 
-    val size_x : Dp  = 4.dp
-    val size_y : Dp = 4.dp
-    val position_x : Dp = (floor_image_size_width * positionX) - (size_x / 2)
-    val position_y : Dp = (floor_image_size_height * positionY) - (size_y / 2)
+    val sizeX : Dp  = 4.dp
+    val sizeY : Dp = 4.dp
+    val positionX : Dp = (floorImageSizeWidth * positionXPercentage) - (sizeX / 2)
+    val positionY : Dp = (floorImageSizeHeight * positionYPercentage) - (sizeY / 2)
 
 
     Box(modifier = Modifier.pointerInput(Unit) {
@@ -158,14 +129,14 @@ fun MapImageView(
         Image(bitmap = imageBitmap,
             contentDescription = "${floor} image",
             modifier = Modifier
-                .size(width = floor_image_size_width, height = floor_image_size_height))
+                .size(width = floorImageSizeWidth, height = floorImageSizeHeight))
 
         Image(painter = painterResource(id = R.drawable.position_icon),
             contentDescription = "${floor} image",
             modifier = Modifier
                 .alpha(if (floor != positionFloor) 0f else 1f)
-                .size(size_x, size_y)
-                .offset(position_x, position_y)
+                .size(sizeX, sizeY)
+                .offset(positionX, positionY)
                 .graphicsLayer {
                     rotationZ = rotation })
     }
@@ -256,8 +227,8 @@ fun MapView(viewModel: MapViewModel = viewModel()) {
                 floor = viewModel.currentFloor,
                 offset = viewModel.offset,
                 onOffsetChange = viewModel::updateOffset,
-                positionX = positionX,
-                positionY = positionY,
+                positionXPercentage = positionX,
+                positionYPercentage = positionY,
                 positionFloor = positionFloor,
                 rotation = rotation
             )
